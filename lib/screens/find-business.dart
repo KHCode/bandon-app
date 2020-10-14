51c421de4fe7060cfa116a98a04d70866bf03900
KHCode <hillkri@oregonstate.edu>
2020-10-14 15:17:10 -0700

    added boilerplate to each screen file

---
+++ lib/screens/find-business.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FindBusinessScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
