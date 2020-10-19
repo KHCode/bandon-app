e07f5978ee0ccc8a56a2c0ba465565b7295254db
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-10-19 14:11:42 -0600

    Add dynamic content samples

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../widgets/settings_drawer.dart';
 import '../widgets/test_distance_display.dart';
+import '../widgets/test_rss_feed.dart';
+import '../widgets/test_web_scraping.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -24,15 +26,21 @@ class _HomePageState extends State<HomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome!',
-              style: TextStyle(fontSize: 28.0),
-            ),
-            TestDistanceDisplay(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Padding(padding: EdgeInsets.only(top: 20.0)),
+              const Text(
+                'Welcome!',
+                style: TextStyle(fontSize: 28.0),
+              ),
+              TestDistanceDisplay(),
+              const Padding(padding: EdgeInsets.only(top: 700.0)),
+              TestRssFeed(),
+              TestWebScraping()
+            ],
+          ),
         ),
       ),
       endDrawer: SettingsDrawer(),
