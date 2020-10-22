8450905027f9f6707f1d00f85bcccf72d3b04429
KHCode <hillkri@oregonstate.edu>
2020-10-21 22:15:44 -0700

    building out ListView

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+// import 'package:weather/weather.dart';
+// import 'package:geolocator/geolocator.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../widgets/settings_drawer.dart';
 import '../widgets/test_distance_display.dart';
 import '../widgets/test_rss_feed.dart';
@@ -15,6 +17,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
   void initState() {
     super.initState();
   }
@@ -25,23 +28,29 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Padding(padding: EdgeInsets.only(top: 20.0)),
-              const Text(
-                'Welcome!',
-                style: TextStyle(fontSize: 28.0),
-              ),
-              TestDistanceDisplay(),
-              const Padding(padding: EdgeInsets.only(top: 700.0)),
-              TestRssFeed(),
-              TestWebScraping()
-            ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFF05668d), Color(0xFFf0f3bd)],
           ),
         ),
+        child: ListView(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
+            child: Image.asset('assets/images/bandon-logo.png'),
+          ),
+          Image.asset('assets/images/beach-sunset.jpg'),
+          Image.asset('assets/images/bandon-weather-widget.jpg'),
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: Text(
+              'About Bandon Oregon',
+              style: GoogleFonts.roboto(),
+            ),
+          )
+        ]),
       ),
       endDrawer: SettingsDrawer(),
     );
