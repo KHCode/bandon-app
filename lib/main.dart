65b9a2305a7164a3494a02980df1495c64e5c3a3
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-10-25 21:04:50 -0600

    Load schema

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,22 @@ import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
+import 'db/database_manager.dart';
 
-const title = 'Bandon';
 void main() async {
+  const title = 'Bandon';
+  const SCHEMA_PATH = 'assets/schema.sql';
+
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
 
-  runApp(App(title: title, prefs: await SharedPreferences.getInstance()));
+  String schema = await rootBundle.loadString(SCHEMA_PATH);
+  await DatabaseManager.initialize(schema);
+
+  runApp(App(
+    title: title,
+    prefs: await SharedPreferences.getInstance(),
+  ));
 }
