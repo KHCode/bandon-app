2d80c105a6b9be9fa71145296d1a98737030df60
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2020-10-31 11:39:03 -0600

    Handle database upgrades and remove old events

--- lib/db/database_manager.dart
+++ lib/db/database_manager.dart
@@ -4,17 +4,24 @@ import 'event_dto.dart';
 import '../models/event.dart';
 
 class DatabaseManager {
-  static const String DATABASE_FILENAME = 'events.sqlite3.db';
-  static const String SQL_DROP_EVENTS_TABLE = 'DROP TABLE IF EXISTS events;';
-  static const String SQL_INSERT_EVENT = '''INSERT OR REPLACE INTO events(
+  static const DATABASE_FILENAME = 'events.sqlite3.db';
+  static const SQL_DROP_EVENTS_TABLE = 'DROP TABLE IF EXISTS events;';
+  static const SQL_INSERT_EVENT = '''INSERT OR REPLACE INTO events(
         title, description, permalink, startDate, endDate, dateDetails, location, admission, website, contact, email, isFavorite, dateFavorited)
         VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, (SELECT isFavorite FROM events WHERE permalink = ?3), (SELECT dateFavorited FROM events WHERE permalink = ?3));''';
-  static const String SQL_SELECT_EVENTS =
-      'SELECT * FROM events ORDER BY date(startDate);';
-  static const String SQL_SELECT_FAVORITE_EVENTS =
-      'SELECT * FROM events WHERE isFavorite = 1 ORDER BY date(startDate);';
-  static const String SQL_UPDATE_FAVORITE_EVENT =
+  static const SQL_SELECT_EVENTS =
+      'SELECT * FROM events ORDER BY datetime(startDate);';
+  static const SQL_SELECT_ONE_EVENT =
+      'SELECT * FROM events WHERE permalink = ?;';
+  static const SQL_SELECT_FAVORITE_EVENTS =
+      'SELECT * FROM events WHERE isFavorite = 1 ORDER BY datetime(startDate);';
+  static const SQL_UPDATE_FAVORITE_EVENT =
       'UPDATE events SET isFavorite = ?, dateFavorited = ? WHERE permalink = ?;';
+  static const SQL_DELETE_OLD_EVENTS =
+      'DELETE FROM events WHERE datetime(endDate) > datetime(?) AND datetime(endDate) < ?;';
+
+  static const SQL_SELECT_OLD_EVENTS =
+      'SELECT * FROM events WHERE datetime(endDate) > datetime(?) AND datetime(endDate) < ?;';
 
   static DatabaseManager _instance;
 
@@ -28,17 +35,21 @@ class DatabaseManager {
   }
 
   static Future initialize(String schema) async {
-    final db = await openDatabase(DATABASE_FILENAME, version: 1,
-        onCreate: (Database db, int version) {
-      _createTables(db, schema);
-    }, onOpen: (Database db) {
-      _createTables(db, schema);
-    });
+    final db = await openDatabase(
+      DATABASE_FILENAME,
+      version: 2,
+      onCreate: (Database db, int version) {
+        _createTables(db, schema);
+      },
+      onUpgrade: (Database db, int oldVersion, int newVersion) {
+        _createTables(db, schema);
+      },
+    );
     _instance = DatabaseManager._(database: db);
   }
 
   static void _createTables(Database db, String sql) async {
-    // await db.execute(SQL_DROP_EVENTS_TABLE);
+    await db.execute(SQL_DROP_EVENTS_TABLE);
     await db.execute(sql);
   }
 
@@ -69,7 +80,18 @@ class DatabaseManager {
     });
   }
 
+  Future<void> _cleanStaleEvents() async {
+    // Remove events that with an end date that's prior to the current time that
+    // also had an end date that the event collection parsed successfully
+    // (i.e., a date that's greater than 1/1/1).
+    db.transaction((txn) async {
+      await txn.rawDelete(SQL_DELETE_OLD_EVENTS,
+          [DateTime.parse('00010101').toString(), DateTime.now().toString()]);
+    });
+  }
+
   Future<List<Event>> getEvents() async {
+    await _cleanStaleEvents();
     final eventRecords = await db.rawQuery(SQL_SELECT_EVENTS);
     final events = eventRecords.map((record) {
       return Event(
